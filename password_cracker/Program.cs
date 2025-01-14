1ca182d48f67b590f52bf0421f8b3c8392c2f251
annaReichelt <anna.m.reichelt@gmail.com>
2025-01-14 17:22:03 +0100

    demo program

--- password_cracker/Program.cs
+++ password_cracker/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Cybersec;
 
+BruteForcer bf = new BruteForcer();
+String pw = "hello";
+TimeSpan ts = bf.bruteForcePassword(pw, "abcdefghijklmnopqrstuvwxyz");
+
+Console.WriteLine("Time taken to crack" + pw + ": " + ts);
 
 Dictionary dict = new Dictionary();
 Dictionary<String, int> statdict = dict.createDictionary("C:/Users/Snaggle/ProgramminProjects/Cybersec25/cybersec/password_cracker/passwords1000.txt");
